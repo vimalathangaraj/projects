61d727f342e93bbecc7618041a97fd7cd50e5821
vimalathangaraj <vimalathangs203@gmail.com>
2026-03-19 14:19:16 +0530

    Terraform ec2 & s3 project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Terraform EC2 and S3
+
+This project creates:
+- One EC2 instance
+- One S3 bucket
+
+## Steps
+
+```bash
+terraform init
+terraform plan
+terraform apply
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+    description = "AWS region"
+    type        = string
+    default     = "ap-south-1"
+}
+variable "instance_type" {
+    description  = "EC2 instance type"
+    type         = string
+    default      = "t2.micro"
+}
+variable "instance_name" {
+    description   = "EC2 instance name"
+    type          = string
+    default       = "terraform-ec2"
+}
+variable "bucket_name" {
+    description = "Unique S3 bucket name"
+    type        = string
+}
