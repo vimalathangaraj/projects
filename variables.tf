5caab25e4e7d8b429fb8b38facb1e05dc08195df
vimalathangaraj <vimalathangs203@gmail.com>
2026-03-23 12:10:12 +0530

    its done

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
-    description = "AWS region"
-    type        = string
-    default     = "ap-south-1"
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 variable "instance_type" {
-    description  = "EC2 instance type"
-    type         = string
-    default      = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 variable "instance_name" {
-    description   = "EC2 instance name"
-    type          = string
-    default       = "terraform-ec2"
+  description = "EC2 instance name"
+  type        = string
+  default     = "terraform-ec2"
 }
 variable "bucket_name" {
-    description = "Unique S3 bucket name"
-    type        = string
+  description = "Unique S3 bucket name"
+  type        = string
 }
